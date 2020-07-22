96f094cef34f6f4950c2190f73df3514830d50e8
kondakova <e.kondakova@cft.ru>
2020-07-22 23:10:46 +0700

    Интерфейс фигуры, классы фигур

---
+++ Shapes/src/academit/shapes/Rectangle.java
@@ -0,0 +1,31 @@
+package academit.shapes;
+
+public class Rectangle implements Shape {
+    private double firstSide;
+    private double secondSide;
+
+    public Rectangle(double firstSide, double secondSide) {
+        this.firstSide = firstSide;
+        this.secondSide = secondSide;
+    }
+
+    @Override
+    public double getWidth() {
+        return Math.max(firstSide, secondSide);
+    }
+
+    @Override
+    public double getHeight() {
+        return Math.min(firstSide, secondSide);
+    }
+
+    @Override
+    public double getArea() {
+        return firstSide * secondSide;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * (firstSide + secondSide);
+    }
+}
