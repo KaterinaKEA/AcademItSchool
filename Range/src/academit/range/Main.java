a1b4f80fcd356c13e96ae954e99abc100f7c56ae
kondakova <e.kondakova@cft.ru>
2020-08-03 22:29:28 +0700

    Добавление функции разности двух интервалов

--- Range/src/academit/range/Main.java
+++ Range/src/academit/range/Main.java
@@ -9,8 +9,8 @@ public class Main {
         double rangeLength = range.getLength();
         System.out.printf("Длина диапозона от %.2f до %.2f = %.2f%n", range.getFrom(), range.getTo(), rangeLength);
 
-        range.setTo(20);
         range.setFrom(2);
+        range.setTo(20);
         System.out.printf("Диапозон стал от %.2f до %.2f%n", range.getFrom(), range.getTo());
 
         System.out.println("Введите число для проверки принадлежности к диапозону:");
@@ -21,6 +21,8 @@ public class Main {
         System.out.printf("Введённое число внутри диапозона %b%n", isInside);
 
         Range range2 = new Range(10, 30);
+        System.out.printf("Второй диапозон %.2f до %.2f%n", range2.getFrom(), range2.getTo());
+
         Range intersectionRanges = new Range();
         intersectionRanges = intersectionRanges.getIntersectionRanges(range, range2);
 
@@ -31,13 +33,26 @@ public class Main {
         }
 
         Range[] unionRange = new Range[2];
-
         unionRange[0] = new Range();
         unionRange = unionRange[0].getUnionRanges(range, range2);
         System.out.printf("Объединение диапозонов от %.2f до %.2f%n", unionRange[0].getFrom(), unionRange[0].getTo());
 
         if (unionRange.length == 2) {
-            System.out.printf("и от %.2f до %.2f%n, так как диапозоны не пересекаются", unionRange[1].getFrom(), unionRange[1].getTo());
+            System.out.printf("и от %.2f до %.2f, так как диапозоны не пересекаются%n", unionRange[1].getFrom(), unionRange[1].getTo());
+        }
+
+        Range[] rangeDifference = new Range[2];
+        rangeDifference[0] = new Range();
+        rangeDifference = rangeDifference[0].getRangesDifference(range, range2);
+
+        if (rangeDifference == null) {
+            System.out.println("Разность диапозонов равна нулю, так как диапозоны пересекаются");
+        } else {
+            System.out.printf("Разность диапозонов от %.2f до %.2f%n", rangeDifference[0].getFrom(), rangeDifference[0].getTo());
+
+            if (rangeDifference.length == 2) {
+                System.out.printf("и от %.2f до %.2f, так как диапозоны не пересекаются%n", rangeDifference[1].getFrom(), rangeDifference[1].getTo());
+            }
         }
     }
 }
--- Range/src/academit/range/Range.java
+++ Range/src/academit/range/Range.java
@@ -57,4 +57,19 @@ public class Range {
         }
         return rangeArray;
     }
+
+    public Range[] getRangesDifference(Range range1, Range range2) {
+        Range[] rangeArray;
+        if (range1.getFrom() == range2.getTo() && range1.getTo() == range2.getFrom()) {
+            return null;
+        } else if (range1.getFrom() <= range2.getTo() && range1.getTo() >= range2.getFrom()) {
+            rangeArray = new Range[1];
+            rangeArray[0] = new Range(range1.getFrom(), range2.getFrom() - 1);
+        } else {
+            rangeArray = new Range[2];
+            rangeArray[0] = range1;
+            rangeArray[1] = range2;
+        }
+        return rangeArray;
+    }
 }
